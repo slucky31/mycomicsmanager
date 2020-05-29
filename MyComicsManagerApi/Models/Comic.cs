63bd79f558af04b6171a2c5fdfb77d7f90fb3371
DUFAUT Nicolas <nicolas.dufaut@capgemini.com>
2020-05-29 23:53:36 +0200

    API Rest Comics & Docker

---
+++ MyComicsManagerApi/Models/Comic.cs
@@ -0,0 +1,20 @@
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace MyComicsManagerApi.Models
+{
+    public class Comic
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        public string Title { get; set; }
+
+        public decimal Price { get; set; }
+
+        public string Category { get; set; }
+
+        public string Author { get; set; }
+    }
+}
