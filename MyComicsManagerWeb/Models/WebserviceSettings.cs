a673cfc9c523ab78d2258f8ab1a7b495a4c7fcc4
Nicolas DUFAUT <nicolas.dufaut@gmail.com>
2020-10-04 00:15:44 +0200

    Web : Ajout lib

--- MyComicsManagerWeb/Models/WebserviceSettings.cs
+++ MyComicsManagerWeb/Models/WebserviceSettings.cs
@@ -3,11 +3,15 @@ namespace MyComicsManagerWeb.Models
     public class WebserviceSettings : IWebserviceSettings
     {
         public string WebserviceUri { get; set; }
+
+        public string LibrariesRootPath { get; set; }
         
     }
 
     public interface IWebserviceSettings
     {
         string WebserviceUri { get; set; }
+
+        string LibrariesRootPath { get; set; }
     }
 }
