7d805291bdcfc849b418c2d351e844baa604977d
Nicolas DUFAUT <nicolas.dufaut@gmail.com>
2021-01-16 22:46:27 +0100

    Ajout des nouveaux champs dans l'API et d'une classe ComicInfo qui pourrait servir plus tard ...

--- MyComicsManagerApi/Models/Comic.cs
+++ MyComicsManagerApi/Models/Comic.cs
@@ -1,3 +1,4 @@
+using System;
 using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
 
@@ -5,23 +6,50 @@ namespace MyComicsManagerApi.Models
 {
     public class Comic
     {
+        
+        // Technical data
+        
         [BsonId]
         [BsonRepresentation(BsonType.ObjectId)]
         public string Id { get; set; }
-
-        public string Title { get; set; }
-
-        public decimal Price { get; set; }
-
-        public string Category { get; set; }
-
-        public string Author { get; set; }
 
         public string EbookPath { get; set; }
 
         public string EbookName { get; set; }
 
         public string LibraryId { get; set; }
+
+        // Book info
+
+        public string Series { get; set; }
+        
+        public string Title { get; set; }
+
+        public string Vomume { get; set; }
+
+        public string Summary { get; set; }
+
+        public decimal Price { get; set; }
+
+        public string Category { get; set; }
+
+        public DateTime Published { get; set; }
+        
+        public string Author { get; set; }
+
+        public string Writer { get; set; }
+
+        public string Penciller { get; set; }
+
+        public string Colorist { get; set; }
+
+        public string Dditor { get; set; }
+
+        public string languageISO { get; set; }
+        
+        public int pageCount { get; set; }
+
+        public int review { get; set; }
 
     }
 }
---
+++ MyComicsManagerApi/Models/ComicInfo.cs
@@ -0,0 +1,44 @@
+namespace MyComicsManagerApi.Models
+{
+    public class ComicInfo
+    {
+        public string series { get; set; }
+        public string title { get; set; }
+        public string number { get; set; }
+        public int count { get; set; }
+        public int volume { get; set; }
+        public string altseries { get; set; }
+        public string altnumber { get; set; }
+        public int altCount  { get; set; }
+        public string summary { get; set; }
+        public string notes { get; set; }
+        public int year  { get; set; }
+        public int month  { get; set; }
+        public int day  { get; set; }
+        public string publisher { get; set; }
+        public string imprint { get; set; }
+        public string genre { get; set; }
+        public string web { get; set; }
+        public string languageISO { get; set; }
+        public string format { get; set; }
+        public string manga { get; set; }
+        public string characters { get; set; }
+        public string teams { get; set; }
+        public string locations { get; set; }
+        public int pageCount { get; set; }
+        public string scanInformation { get; set; }
+        public string storyArc { get; set; }
+        public string seriesGroup { get; set; }
+        public string ageRating { get; set; }
+        public string blackandwhite { get; set; }
+        public string review { get; set; }
+        public string writer { get; set; }
+        public string penciller { get; set; }
+        public string inker { get; set; }
+        public string colorist { get; set; }
+        public string letterer { get; set; }
+        public string coverArtist { get; set; }
+        public string editor { get; set; }
+        public string mainCharacterOrTeam { get; set; }
+    }
+}
