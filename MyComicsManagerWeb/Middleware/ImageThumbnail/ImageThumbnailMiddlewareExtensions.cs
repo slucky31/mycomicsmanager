62a8c7d2fb57c362812c36c35d002e14c44526e6
Nicolas DUFAUT <nicolas.dufaut@gmail.com>
2021-05-06 16:13:45 +0200

    #53 : Correction Build DockerHub (#54)

---
+++ MyComicsManagerWeb/Middleware/ImageThumbnail/ImageThumbnailMiddlewareExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Builder;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ImageThumbnail.AspNetCore.Middleware
+{
+    public static class ImageThumbnailMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseImageThumbnail(
+            this IApplicationBuilder builder,ImageThumbnailOptions options)
+        {
+            return builder.UseMiddleware<ImageThumbnailMiddleware>(options);
+        }
+    }
+}
