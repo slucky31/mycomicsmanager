bdcca0024467be2363513982bcdcd17485c39d02
Nicolas DUFAUT <nicolas.dufaut@gmail.com>
2021-08-24 13:36:31 +0200

    #20 : Correction Pb Génération miniatures
    Fixes #20

--- Middleware/ImageThumbnail/ImageThumbnailMiddlewareExtensions.cs
+++ Middleware/ImageThumbnail/ImageThumbnailMiddlewareExtensions.cs
@@ -1,15 +1,22 @@
 ﻿using Microsoft.AspNetCore.Builder;
-using System;
-using System.Collections.Generic;
-using System.Text;
+using Microsoft.Extensions.FileProviders;
+using MyComicsManagerWeb.Models;
 
 namespace ImageThumbnail.AspNetCore.Middleware
 {
     public static class ImageThumbnailMiddlewareExtensions
     {
         public static IApplicationBuilder UseImageThumbnail(
-            this IApplicationBuilder builder,ImageThumbnailOptions options)
+            this IApplicationBuilder builder, string coversDirRootPath, ImageThumbnailOptions options)
         {
+            
+            // Lien entre /covers et le directory sur le disque
+            //builder.UseStaticFiles(new StaticFileOptions
+            //{
+            //    FileProvider = new PhysicalFileProvider(coversDirRootPath),
+            //    RequestPath = "/covers"
+            //});
+
             return builder.UseMiddleware<ImageThumbnailMiddleware>(options);
         }
     }
