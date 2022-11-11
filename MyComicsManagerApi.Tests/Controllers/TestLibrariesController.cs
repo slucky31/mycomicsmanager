167414a951cf98c20982a3c75fe922b78b001af0
Nicolas DUFAUT <nicolas.dufaut@gmail.com>
2022-11-11 23:15:08 +0100

    Test : la suite !

--- MyComicsManagerApi.Tests/Controllers/TestLibrariesController.cs
+++ MyComicsManagerApi.Tests/Controllers/TestLibrariesController.cs
@@ -3,6 +3,7 @@ using FluentAssertions;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Moq;
+using MyComicsManager.Model.Shared.Models;
 using MyComicsManagerApi.Controllers;
 using MyComicsManagerApi.Services;
 using MyComicsManagerApiTests.MockData;
@@ -26,7 +27,51 @@ public class TestLibrariesController
         
         // Assert
         actionResult.Should().NotBeNull();
-        actionResult.Value.Count.Should().Be(4);
+        actionResult.Value.Should().NotBeNull();
+        actionResult.Value!.Count.Should().Be(4);
+        return Task.CompletedTask;
+    }
+    
+    [Fact]
+    public Task GetId_ShouldReturn200Status()
+    {
+        // Arrange
+        var libService = new Mock<ILibraryService>();
+        var mockLogger = new Mock<ILogger<LibrariesController>>();
+        libService.Setup(_ => _.Get("1")).Returns(LibraryMockData.GetId("1"));
+        var sut = new LibrariesController(mockLogger.Object, libService.Object);
+ 
+        // Act
+        var actionResult = sut.Get("1");
+        
+        // Assert
+        actionResult.Should().NotBeNull();
+        actionResult.Value.Should().NotBeNull();
+        var lib = actionResult.Value;
+        lib!.Id.Should().Be("1");
+        lib!.RelPath.Should().Be("lib1");
+        lib!.Name.Should().Be("Library 1");
+        return Task.CompletedTask;
+    }
+    
+    [Fact]
+    public Task GetId_ShouldReturn404Status()
+    {
+        // Arrange
+        var libService = new Mock<ILibraryService>();
+        var mockLogger = new Mock<ILogger<LibrariesController>>();
+        libService.Setup(_ => _.Get("1")).Returns((Library) null);
+        var sut = new LibrariesController(mockLogger.Object, libService.Object);
+ 
+        // Act
+        var actionResult = sut.Get("1");
+        
+        // Assert
+        actionResult.Should().NotBeNull();
+        actionResult.Result.Should().NotBeNull();
+        actionResult.Result.Should().BeOfType<NotFoundResult>();
+        var res = (NotFoundResult)actionResult.Result;
+        res!.StatusCode.Should().Be(404);
         return Task.CompletedTask;
     }
 }
--- MyComicsManagerApi.Tests/MockData/LibraryMockData.cs
+++ MyComicsManagerApi.Tests/MockData/LibraryMockData.cs
@@ -12,26 +12,41 @@ public static class LibraryMockData
             new Library
             {
                 Id = "1",
+                Name = "Library 1",
                 RelPath = "lib1"
             });
         libs.Add(
             new Library
             {
                 Id = "2",
+                Name = "Library 2",
                 RelPath = "lib2"
             });
         libs.Add(
             new Library
             {
                 Id = "3",
+                Name = "Library 3",
                 RelPath = "lib3"
             });
         libs.Add(
             new Library
             {
                 Id = "4",
+                Name = "Library 4",
                 RelPath = "lib4"
             });
         return libs;
+    }
+    
+    public static Library GetId(string id)
+    {
+        return
+            new Library
+            {
+                Id = id,
+                Name = "Library 1",
+                RelPath = "lib1"
+            };
     }
 }
