0bbcda7aaf2a5f9ab8ed2e180fca539b661b35b9
Nicolas Dufaut <nicolas.dufaut@gmail.com>
2023-12-30 19:04:04 +0100

    fix: debug

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -42,11 +42,8 @@ builder.Services.AddProblemDetails();
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseSerilogRequestLogging();
 
