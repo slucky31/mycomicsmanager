6393fada6cb5ba557deb5044b2da9714ac3760ec
Nicolas Dufaut <nicolas.dufaut@gmail.com>
2024-02-03 10:44:32 +0100

    fix: UnitTest StringExtensions

---
+++ Domain.UnitTests/Primitives/ResultTests.cs
@@ -0,0 +1,69 @@
+﻿using Domain.Primitives;
+
+namespace Domain.UnitTests.Primitives;
+
+public class ResultTests
+{
+    // Arrange
+    // Act
+    // Assert
+
+    [Fact]
+    public void Success_Should_SetIsSuccesToTrue()
+    {
+        // Arrange
+        // Act
+        var result = Result.Success();
+
+        // Assert
+        result.IsSuccess.Should().Be(true);
+        result.IsFailure.Should().Be(false);
+        result.Error.Should().BeNull();
+    }
+
+    [Fact]
+    public void Failure_Should_SetIsFailureToTrue()
+    {
+        // Arrange
+        var error = new TError("500", "BSOD");
+
+        // Act
+        var result = Result.Failure(error);
+
+        // Assert
+        result.IsSuccess.Should().Be(false);
+        result.IsFailure.Should().Be(true);
+        result.Error.Should().Be(error);
+    }
+
+    [Fact]
+    public void Implicit_Should_ConvertFromTError()
+    {
+        // Arrange
+        var error = new TError("500", "BSOD");
+
+        // Act
+        Result result = error;
+
+        // Assert
+        result.IsSuccess.Should().Be(false);
+        result.IsFailure.Should().Be(true);
+        result.Error.Should().Be(error);
+    }
+
+    [Fact]
+    public void ToResult_Should_ConvertFromTError()
+    {
+        // Arrange
+        var error = new TError("500", "BSOD");
+
+        // Act
+        var result = Result.ToResult(error);
+
+        // Assert
+        result.IsSuccess.Should().Be(false);
+        result.IsFailure.Should().Be(true);
+        result.Error.Should().Be(error);
+    }
+
+}
