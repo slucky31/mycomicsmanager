e9b1fc03c174cc61d5a61cc38ba869b956ed29d3
Nicolas Dufaut <nicolas.dufaut@gmail.com>
2024-03-23 09:57:08 +0100

    fix : realy delete WebApi project

--- WebAPI/Options/MongoDbOptions.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace WebAPI.Options;
-
-public class MongoDbOptions
-{
-    public string ConnectionString { get; init; } = string.Empty;
-    public string DatabaseName { get; init; } = string.Empty;
-}
