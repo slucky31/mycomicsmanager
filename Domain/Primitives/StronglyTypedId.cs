d67554fbd5c5be12ea5fccb573dde8aa8fc2e089
Nicolas Dufaut <nicolas.dufaut@gmail.com>
2024-05-12 14:53:30 +0200

    fix: delete unused file

--- Domain/Primitives/StronglyTypedId.cs
+++
@@ -1,7 +0,0 @@
-﻿using MongoDB.Bson;
-
-namespace Domain.Primitives;
-
-public record StronglyObjectIdTypedId(ObjectId Id) : StronglyTypedId<ObjectId>(Id);
-
-public abstract record StronglyTypedId<TValue>(TValue Id) where TValue : notnull;
