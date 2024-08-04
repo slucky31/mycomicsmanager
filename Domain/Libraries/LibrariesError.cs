2945e8bd44cfbd0cae3ff89fcff615e9f33301b1
Nicolas Dufaut <nicolas.dufaut@gmail.com>
2024-08-04 22:53:40 +0200

    Modify Update Lib

--- Domain/Libraries/LibrariesError.cs
+++ Domain/Libraries/LibrariesError.cs
@@ -8,4 +8,5 @@ public static class LibrariesError
     public static readonly TError Duplicate = new("LIB409", "A librabry is already created with this name");
     public static readonly TError FolderNotCreated = new("LIB501", "The library directory can not be created");
     public static readonly TError FolderNotDeleted = new("LIB502", "The library directory was not deleted");
+    public static readonly TError FolderNotMoved = new("LIB503", "The library directory was not moved");
 }
