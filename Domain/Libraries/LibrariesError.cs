944541f995d38744b904a8820ae01b830ee1a226
Nicolas Dufaut <nicolas.dufaut@gmail.com>
2024-08-04 22:20:14 +0200

    Delete library

--- Domain/Libraries/LibrariesError.cs
+++ Domain/Libraries/LibrariesError.cs
@@ -6,5 +6,6 @@ public static class LibrariesError
     public static readonly TError BadRequest = new("LIB400", "Verify the parameter of the request");
     public static readonly TError NotFound = new("LIB404", "Library not found");    
     public static readonly TError Duplicate = new("LIB409", "A librabry is already created with this name");
-    public static readonly TError FolderNotCreated = new("LIB500", "The library directory can not be created");
+    public static readonly TError FolderNotCreated = new("LIB501", "The library directory can not be created");
+    public static readonly TError FolderNotDeleted = new("LIB502", "The library directory was not deleted");
 }
--- Domain/Primitives/TError.cs
+++ Domain/Primitives/TError.cs
@@ -3,4 +3,5 @@
 public sealed record TError(string Code, string? Description = null)
 {
     public static readonly TError None = new(string.Empty);
+    public static readonly TError Any = new("Any");
 }
