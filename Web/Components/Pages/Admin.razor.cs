48d1b4f92c51edf1e0f82148c65c9d6c7ab6441d
Nicolas Dufaut <nicolas.dufaut@gmail.com>
2026-02-23 19:31:41 +0100

    Make "picture" claim lookup case-sensitive and safer

--- Web/Components/Pages/Admin.razor.cs
+++ Web/Components/Pages/Admin.razor.cs
@@ -19,8 +19,8 @@ public partial class Admin
 
             Username = state?.User?.Identity?.Name ?? string.Empty;
 
-            Picture = state?.User.Claims
-                        .Where(c => c.Type.Equals("picture"))
+            Picture = state?.User?.Claims?
+                        .Where(c => c.Type.Equals("picture", StringComparison.Ordinal))
                         .Select(c => c.Value)
                         .FirstOrDefault() ?? string.Empty;
         }
