4abc1b2f0d9cbecab4f60dfb570060f3316da302
Nicolas Dufaut <nicolas.dufaut@gmail.com>
2026-02-24 11:26:37 +0100

    Persist background image selection in Login component

--- Web/Components/Pages/Login.razor.cs
+++ Web/Components/Pages/Login.razor.cs
@@ -4,27 +4,45 @@ using Serilog;
 
 namespace Web.Components.Pages;
 
-public partial class Login
+public partial class Login : IDisposable
 {
-
     [Inject] private IWebHostEnvironment WebHostEnvironment { get; set; } = default!;
-    private string[]? _allBackgroundImages;
+    [Inject] private PersistentComponentState ApplicationState { get; set; } = default!;
 
+    private string[]? _allBackgroundImages;
     private string? _selectedImage;
     private const int MaxFiles = 10;
+    private PersistingComponentStateSubscription _persistingSubscription;
 
     protected override void OnInitialized()
     {
-        _allBackgroundImages ??= LoadBackgroundImages(WebHostEnvironment.WebRootPath);
-        var images = _allBackgroundImages;
-        if (images.Length == 0)
-        {
-            _selectedImage = string.Empty;
-            return;
-        }
+        _persistingSubscription = ApplicationState.RegisterOnPersisting(PersistSelectedImage);
 
-        _selectedImage = images[RandomNumberGenerator.GetInt32(images.Length)];
+        if (!ApplicationState.TryTakeFromJson<string>(nameof(_selectedImage), out var restored))
+        {
+            _allBackgroundImages ??= LoadBackgroundImages(WebHostEnvironment.WebRootPath);
+            var images = _allBackgroundImages;
+            if (images.Length == 0)
+            {
+                _selectedImage = string.Empty;
+                return;
+            }
+
+            _selectedImage = images[RandomNumberGenerator.GetInt32(images.Length)];
+        }
+        else
+        {
+            _selectedImage = restored;
+        }
     }
+
+    private Task PersistSelectedImage()
+    {
+        ApplicationState.PersistAsJson(nameof(_selectedImage), _selectedImage);
+        return Task.CompletedTask;
+    }
+
+    public void Dispose() => _persistingSubscription.Dispose();
 
     private static string[] LoadBackgroundImages(string webRootPath)
     {
